4d46531d7959a84dd91f082d8a0bdaa1cb629eeb
thilipraj38 <rtbraj38@gmail.com>
2023-12-14 18:52:43 +0530

    Print statement changed

--- src/com/sample/DataOne.java
+++ src/com/sample/DataOne.java
@@ -4,7 +4,7 @@ public class DataOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello moto");
+		System.out.println("Hello moto ggggg");
 	}
 
 }
